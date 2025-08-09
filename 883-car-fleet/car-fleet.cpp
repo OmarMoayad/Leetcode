c81cddcce92d0392602e9352065844c080300670
OmarMoayad <s12220112@stu.najah.edu>
2025-08-09 05:28:42 +0300

    Time: 54 ms (42.99%) | Memory: 84.4 MB (56.78%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         int n = position.size();
-        vector<pair<int, double>> cars(n);  // pair: (position, time to reach target)
+        vector<pair<int, double>> cars(n);  
         
         for (int i = 0; i < n; ++i) {
             double time = (double)(target - position[i]) / speed[i];
             cars[i] = {position[i], time};
         }
         
-        // Sort cars by starting position descending (closest to target first)
         sort(cars.rbegin(), cars.rend());
         
         int fleets = 0;
@@ -17,11 +16,10 @@ public:
         
         for (auto& car : cars) {
             if (car.second > currentFleetTime) {
-                // New fleet forms if this car takes longer than current fleet
                 fleets++;
                 currentFleetTime = car.second;
             }
-            // Otherwise, this car joins the fleet with currentFleetTime
+
         }
         
         return fleets;
